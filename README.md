e5cef655af58a77c563748e224688839154a55d2
ajayb <ajay.bhargov@gmail.com>
2017-01-24 09:18:54 -0800

    [SpringBoot] - Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Hello World Spring Boot Application
+
+## Building the project
+
+    ./gradlew clean
+    ./gradlew build
+
+## Running the project
+
+    java -jar build/libs/springboot-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/com/ajayb/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.ajayb;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Hello World";
+    }
+}
